80cfcf16d9d3e5c419b72a2c4e19ce4c94614e6f
Steve Tjoa <kiemyang@gmail.com>
2014-03-27 23:52:59 -0700

    new tests

---
+++ test.py
@@ -0,0 +1,9 @@
+import numpy
+import scipy
+import matplotlib
+import pandas
+
+print 'NumPy version %s', numpy.version.version
+print 'SciPy version %s', scipy.version.version
+print 'Matplotlib version %s', matplotlib.__version__
+print 'Pandas version %s', pandas.version.version

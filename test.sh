ad073995402eeac12f130cd7bf78fb8c2e4000b7
Steve Tjoa <kiemyang@gmail.com>
2014-03-29 17:11:57 -0700

    adding pip install pip

--- test.sh
+++ test.sh
@@ -3,14 +3,15 @@
 # install pip
 apt-get -qq update
 apt-get -y install python-dev python-pip
+pip install pip
 
 cd /vagrant
 python --version
 
 # taken from .travis.yml
 pwd
-pip install ansible
-ansible-galaxy install stevetjoa.python-common,1.01 --roles-path=.
+/usr/local/bin/pip install ansible
+ansible-galaxy install stevetjoa.python-common --roles-path=.
 ansible-playbook -i hosts --connection=local test.yml
 python test.py
 ipython --version
